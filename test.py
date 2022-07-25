a5f4945143df523451f310ee39926e216d7e6a0a
Alex Thompson <alex@arcticdataservices.com>
2022-07-25 05:14:30 -0700

    Add test for forbidden_for_transport

--- test.py
+++ test.py
@@ -26,3 +26,14 @@ class Product_TestCase(unittest.TestCase):
             self.assertEqual(product.regulatory_definition, '49 CFR 173.128(a)(1)')
         with self.subTest():
             self.assertEqual(product.is_organic_peroxide, False)
+            
+    def test_is_forbidden_for_transport(self):
+        '''Test setting the Product attribute and confirm the regulatory definition is correctly applied'''
+
+        product = Product(name='Product 1', is_forbidden_for_transport = True)
+        with self.subTest():
+            self.assertEqual(product.is_forbidden_for_transport, True)
+        with self.subTest():
+            self.assertEqual(product.regulatory_definition, '49 CFR 173.128(a)(2)')
+        with self.subTest():
+            self.assertEqual(product.is_organic_peroxide, False)
