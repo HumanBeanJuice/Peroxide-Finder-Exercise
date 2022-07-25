61e374c9b4c0c3006d3f05e81ae76fab24e28e0b
Alex Thompson <alex@arcticdataservices.com>
2022-07-25 04:56:36 -0700

    Read in .csv with pandas and rename columns

--- app.py
+++ app.py
@@ -4,3 +4,19 @@ from dataclasses import dataclass, field
 import numpy as np
 import pandas as pd
 
+# Add SMILES and MW data from PubChem to data .csv file
+
+# Read in the .csv file using Pandas
+df = pd.read_csv('Product_List.csv', engine='python')
+
+# Rename columns to make them easier to work with
+df.rename(columns=
+    {
+        'Product ID':'product_id',
+        'CAS':'cas',
+        'SMILES':'smiles',
+        'Chemical Name':'name', 
+        'Molecular Weight':'mw', 
+        'min (%)':'min_conc',
+        'max (%)':'max_conc'
+    }, inplace=True)
