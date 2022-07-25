32aa98d8a722b843b137536f1e76d10e8cb92ccd
Alex Thompson <alex@arcticdataservices.com>
2022-07-25 05:13:58 -0700

    Add test for is_explosive to test calc func

--- test.py
+++ test.py
@@ -15,3 +15,14 @@ class Product_TestCase(unittest.TestCase):
             self.assertEqual(product.is_forbidden_for_transport, False)
         with self.subTest():
             self.assertEqual(product.is_organic_peroxide, True)
+
+    def test_is_explosive(self):
+        '''Test setting the Product attribute and confirm the regulatory definition is correctly applied'''
+
+        product = Product(name='Product 1', is_explosive=True)
+        with self.subTest():
+            self.assertEqual(product.is_explosive, True)
+        with self.subTest():
+            self.assertEqual(product.regulatory_definition, '49 CFR 173.128(a)(1)')
+        with self.subTest():
+            self.assertEqual(product.is_organic_peroxide, False)
